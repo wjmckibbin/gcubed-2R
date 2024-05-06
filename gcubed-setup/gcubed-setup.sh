74e2fb9a039756466173378d4f6dfc19e6a25639
AshieSlashy <a.aschoff@gmail.com>
2024-05-07 02:29:19 +1000

    Try mapping in .ssh

--- gcubed-setup/gcubed-setup.sh
+++ gcubed-setup/gcubed-setup.sh
@@ -68,9 +68,9 @@ echo "**************************************************************************
 # First check for availability of user data
 enter_directory "${user_data_directory}"
 
-echo "Adding github.com to known hosts"
-# if [ ! "$(ssh-keygen -F github.com)" ]; then ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null; fi
-ssh-keyscan github.com > ~/.ssh/known_hosts 2>/dev/null
+# echo "Adding github.com to known hosts"
+# # if [ ! "$(ssh-keygen -F github.com)" ]; then ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null; fi
+# ssh-keyscan github.com > ~/.ssh/known_hosts 2>/dev/null
 
 echo "Looking for user data repository at: ${user_data_repository}"
 
