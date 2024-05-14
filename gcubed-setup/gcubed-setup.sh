6c9b076898f1339cf8a3eb845a5a068724cc079b
AshieSlashy <a.aschoff@gmail.com>
2024-05-14 21:15:35 +1000

    Switched to using main Sym repo.  Cleaned up & simplified directory structure. Brought install script to alignment with template

--- gcubed-setup/gcubed-setup.sh
+++ gcubed-setup/gcubed-setup.sh
@@ -17,10 +17,15 @@ function enter_directory() {
     echo "Changed directory from: ${OLDPWD} -> ${PWD}"
 }
 
-while getopts "p:s:t:u:v:" flag
+function show_current_commit() {
+    echo "${1}$(git --no-optional-locks symbolic-ref --short HEAD 2>/dev/null || git --no-optional-locks rev-parse --short HEAD 2>/dev/null)"
+}
+
+while getopts "g:p:s:t:u:v:" flag
 do
     case "${flag}" in
-        p) prereq_repository="${OPTARG}";;
+        g) sym_repository_tag="${OPTARG}";;
+        p) gcubed_prerequisites_repository="${OPTARG}";;
         s) sym_repository="${OPTARG}";;
         t) temp_directory="${OPTARG}";;
         u) user_data_defaults_directory="${OPTARG}";;
@@ -28,8 +33,7 @@ do
     esac
 done
 
-parent_repository="${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}"
-prereq_temp_directory="${temp_directory}/$(directory_from_repo_url $prereq_repository)"
+gcubed_prerequisites_temp_directory="${temp_directory}/$(directory_from_repo_url $gcubed_prerequisites_repository)"
 sym_source_temp_directory="${temp_directory}/$(directory_from_repo_url $sym_repository)"
 sym_executable_directory="/usr/local/bin/"
 
@@ -38,15 +42,15 @@ echo "User info: $(id)"
 echo -e "Machine info: $(uname -a)\n"
 echo "Current directory: ${PWD}"
 echo -e "In codespace: ${CODESPACES}\n"
-echo "Parent repository: ${parent_repository}"
 echo "User data defaults source directory: ${user_data_defaults_directory}"
 echo "Temp directory: ${temp_directory}"
 echo -e "Sym executable directory: ${sym_executable_directory}\n"
 echo -e "User data directory: ${user_data_directory}\n"
-echo -e "Prerequisites repository: ${prereq_repository}"
-echo -e "Prerequisites repo directory: ${prereq_temp_directory}\n"
-echo -e "Sym repository: ${sym_repository}"
-echo -e "Sym repo directory: ${sym_source_temp_directory}\n"
+echo "Prerequisites repository: ${gcubed_prerequisites_repository}"
+echo -e "Prerequisites repo directory: ${gcubed_prerequisites_temp_directory}\n"
+echo "Sym repository: ${sym_repository}"
+echo "Sym repository TAG: ${sym_repository_tag}"
+echo -e "Sym source temporary directory: ${sym_source_temp_directory}\n"
 echo -e "*****************************************************************************************\n"
 
 echo "User data directory (${user_data_directory}) listing:"
@@ -61,9 +65,7 @@ cp -r --no-clobber ${user_data_defaults_directory}/. ${user_data_directory}
 # Install gcubed prereqs
 echo "Pulling & installing prerequisites"
 
-echo "Creating temporary directory ("${temp_directory}")..."
-sudo sudo install -d -m 775 -o vscode -g root  "${temp_directory}"
-
+echo "Updating apt & installing byacc"
 # yacc/byacc required by sym 
 # doing upfront in case the update affects any python prerequisites?
 # I'd prefer to bake byacc & the path changes into the docker image but... macOS... :/
@@ -72,10 +74,15 @@ sudo apt-get clean
 sudo apt-get update
 sudo apt-get -y install byacc
 
+echo "Creating temporary directory ("${temp_directory}")..."
+sudo sudo install -d -m 775 -o vscode -g root  "${temp_directory}"
 enter_directory "${temp_directory}"
-git clone "${prereq_repository}" "${prereq_temp_directory}"
 
-enter_directory "${prereq_temp_directory}"
+echo "Installing G-Cubed code"
+git clone --depth 1 --single-branch "${gcubed_prerequisites_repository}" "${gcubed_prerequisites_temp_directory}"
+
+enter_directory "${gcubed_prerequisites_temp_directory}"
+show_current_commit "Pulled gcubed code commit: "
 
 echo "Installing any whl files"
 for F in *.whl; do
@@ -100,10 +107,13 @@ for F in req*.txt; do
     rm "$F"
 done
 
+echo "Installing sym processor"
 enter_directory ".."
-git clone "${sym_repository}" "${sym_source_temp_directory}"
+# git clone "${sym_repository}" "${sym_source_temp_directory}"
+git clone --depth 1 --single-branch --branch "${sym_repository_tag}" "${sym_repository}" "${sym_source_temp_directory}"
 
 enter_directory "${sym_source_temp_directory}/src"
+show_current_commit "Pulled sym code commit: "
 
 make
 chmod a-w sym
